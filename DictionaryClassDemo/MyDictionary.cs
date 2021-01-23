067666972260e799393f6b9b16a4d2f7bccb793f
MeralTd <meraltasdemir77@gmail.com>
2021-01-23 14:38:15 +0300

    Add Dictionary Class

---
+++ DictionaryClassDemo/MyDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryClassDemo
+{
+    class MyDictionary<T>
+    {
+        T[] items;
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+        }
+    }
+}
---
+++ DictionaryClassDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DictionaryClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<string> names = new MyDictionary<string>();
+            names.Add("Meral");
+            names.Add("Ayşe");
+            names.Add("Sümeyye");
+            names.Add("Şevval");
+            names.Add("Kadriye");
+
+            Console.WriteLine(names.Count);
+            
+
+        }
+
+
+    }
+
+
+}
